dfa55cadf5afd605f3a733509d2aafe399aa466b
lyh221 <lyh221@naver.com>
2014-07-27 21:07:51 +0900

    0727

--- tripwithme/src/com/example/tripwithme/menu.java
+++ tripwithme/src/com/example/tripwithme/menu.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.content.*;
 import android.graphics.*;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,23 +15,19 @@ import android.widget.*;
 import android.os.Build;
 
 public class menu extends Activity{
-	
-	Typeface mFont;
-	
+    
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
-        
-        mFont=Typeface.createFromAsset(getAssets(), "fonts/FinenessProBlack.otf");
+
         Button mapbtn = (Button)findViewById(R.id.MAP);
         Button resbtn = (Button)findViewById(R.id.RESTAURENT);
         Button tourbtn = (Button)findViewById(R.id.TOUR);
-        mapbtn.setTypeface(mFont);
-        resbtn.setTypeface(mFont);
-        tourbtn.setTypeface(mFont);              
+        
         
     }
-
+	
+	
 	public void mOnClick(View v) {
 		switch(v.getId()) {
 		case R.id.MAP :
