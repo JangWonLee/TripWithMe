dd478027dd3c283f54074bce6d2c6d793bc2278d
JangWonLee <wonnna1021@gmail.com>
2014-08-07 00:11:47 +0900

    project UPDATE / map.java -> not yet

--- tripwithme/src/com/example/tripwithme/RestaurantDetail.java
+++ tripwithme/src/com/example/tripwithme/RestaurantDetail.java
@@ -15,47 +15,50 @@ import android.view.ViewGroup;
 import android.widget.*;
 import android.os.Build;
 
-public class TourDetail extends Activity {
-	private TourDBHelper mHelper;
-
+public class RestaurantDetail extends Activity {
+	private RestaurantDBHelper mHelper;
+	
 	private String name;
 	private String intro;
 	private String time;
 	private String tel;
 	private String menu;
-
+	
 	private TextView listDetailTitleText;
 	private TextView introText;
 	private TextView timeText;
 	private TextView telephoneText;
 	private TextView listDetailMenuText;
-
+	
 	private Button locationButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.listdetail);		      
+		setContentView(R.layout.listdetail);	
 
 		locationButton = (Button)findViewById(R.id.locationbutton);
 		
 		name = getIntent().getExtras().getString("name");
 		intro = getIntent().getExtras().getString("intro");
+		time = getIntent().getExtras().getString("time");
 		tel = getIntent().getExtras().getString("tel");
 		menu = getIntent().getExtras().getString("menu");
 
 
 		listDetailTitleText = (TextView)findViewById(R.id.listdetailtitletext);
 		introText=(TextView)findViewById(R.id.introtext);
+		timeText=(TextView)findViewById(R.id.timetext);
 		telephoneText=(TextView)findViewById(R.id.telephonetext);
 		listDetailMenuText=(TextView)findViewById(R.id.listdetailmenutext);
 
 		listDetailTitleText.setText(name);
 		introText.setText(intro);
+		timeText.setText(time);
 		telephoneText.setText(tel);
 		listDetailMenuText.setText(menu);
 	}
-	
+
 	public void mOnClick(View v){
 		Intent intent = new Intent(this, Map.class);
 		startActivity(intent);
